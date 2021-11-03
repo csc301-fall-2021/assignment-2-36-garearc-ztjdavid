2a44427953de5baf83977e0d8f7d29139aa9ba70
GareArc <cxy329288574@gmail.com>
2021-11-03 01:11:28 -0400

    Uploaded starter files.

---
+++ src/test/java/com/csc301group36/Covid19API/Covid19ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csc301group36.Covid19API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
